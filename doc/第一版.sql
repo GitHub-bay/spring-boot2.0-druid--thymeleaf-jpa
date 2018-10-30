8324b134237eb21be30d43711f74393887715f49
MOEKIRITO <mzhy08@gmail.com>
2018-10-30 09:24:49 +0800

    修改SQL。

--- doc/第一版.sql
+++ doc/第一版.sql
@@ -1,24 +1,11 @@
 -- ----------------------------
--- Table structure for hibernate_sequence
--- ----------------------------
-DROP TABLE IF EXISTS `hibernate_sequence`;
-CREATE TABLE `hibernate_sequence`  (
-  `next_val` bigint(20) NULL DEFAULT NULL
-) ENGINE = MyISAM CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Fixed;
-
--- ----------------------------
--- Records of hibernate_sequence
--- ----------------------------
-INSERT INTO `hibernate_sequence` VALUES (14);
-
--- ----------------------------
 -- Table structure for users
 -- ----------------------------
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users`  (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `USER_NAME` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
-  `PASSWORD` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `USER_NAME` varchar(255) NOT NULL,
+  `PASSWORD` varchar(255) NOT NULL,
   `AGE` int(11) NOT NULL,
   PRIMARY KEY (`ID`) USING BTREE
 )
@@ -27,9 +14,9 @@ CREATE TABLE `users`  (
 -- Records of users
 -- ----------------------------
 INSERT INTO `users` VALUES (1, 'admin', '123456', 10);
-INSERT INTO `users` VALUES (2, 'admin', '123456', 10);
-INSERT INTO `users` VALUES (3, 'admin', '123456', 10);
-INSERT INTO `users` VALUES (4, 'admin', '123456', 10);
-INSERT INTO `users` VALUES (5, 'admin', '123456', 10);
-
-SET FOREIGN_KEY_CHECKS = 1;
+INSERT INTO `users` VALUES (2, 'admin', '12345678', 10);
+INSERT INTO `users` VALUES (3, 'admin', '12345678', 10);
+INSERT INTO `users` VALUES (4, 'admin', '12345678', 10);
+INSERT INTO `users` VALUES (5, 'admin', '12345678', 10);
+INSERT INTO `users` VALUES (6, 'admin', '12345678', 10);
+INSERT INTO `users` VALUES (7, 'admin', '12345678', 10);
